3b9783bbc854526dcf81a6cbfa8dd89b88c8a00c
NguyenHauHuu <huu_dpm225425@student.agu.edu.vn>
2025-07-30 17:00:00 +0700

    Khoi tao csdl

--- QuanLyQuanCaPhe/Program.cs
+++ QuanLyQuanCaPhe/Program.cs
@@ -8,7 +8,7 @@ namespace QuanLyQuanCaPhe
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
+            // To customize application configuration such as set high DPI setti  ngs or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
